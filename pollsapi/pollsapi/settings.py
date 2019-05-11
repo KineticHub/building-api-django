b09fabcea93f4ee76b53d4388961a4d023acf729
Kinetic Alnajar <kalnajar09@gmail.com>
2019-05-11 10:40:57 -0400

    zappa and arurora added, aurora not working

--- pollsapi/pollsapi/settings.py
+++ pollsapi/pollsapi/settings.py
@@ -85,10 +85,20 @@ WSGI_APPLICATION = 'pollsapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pollsdb',  # dbname
+        'USER': 'polls_admin',  # master username
+        'PASSWORD': 'pollsadmin',  # master password
+        'HOST': 'openincentives-qa-cluster.cluster-ckuo2woivn72.us-east-1.rds.amazonaws.com',  # Endpoint
+        'PORT': '3306',
     }
 }
 
